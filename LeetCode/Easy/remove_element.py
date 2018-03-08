a2bd310f49f8ad66a58fda3b7d8dabac109b970f
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-03-08 16:35:50 -0500

    Remove single element which is provided as argument to function, from an array in_place

---
+++ LeetCode/Easy/remove_element.py
@@ -0,0 +1,29 @@
+"""
+Given an array and a value, remove all instances of that value in-place and return the new length.
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+"""
+
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        if not nums:
+            # If the nums array is empty
+            return 0
+        
+        prev_ind = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[prev_ind] = nums[i]
+                prev_ind += 1
+        
+        return prev_ind
