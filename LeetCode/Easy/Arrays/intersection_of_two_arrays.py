88802d5360679e8e99104ab7f6c0c7e632981d7f
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-05-20 07:30:42 -0400

    Intersection of 2 arrays

---
+++ LeetCode/Easy/Arrays/intersection_of_two_arrays.py
@@ -0,0 +1,52 @@
+# RUNTIME -> 46ms
+def intersection(nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+
+    # Design strategy
+    # Put all the elements of 1 array in a dictionary, so taht lookup is
+    # O(1)
+
+    if nums1 is [] and nums2 is []:
+        return []
+
+    # If either one is empty whiel the other is not
+    if nums1 is [] and nums2:
+        return []
+
+    if nums2 is [] and nums1:
+        return []
+
+
+    my_dict = {}
+    final_list = []
+
+    # Put all the elements of nums1 in a dict
+    for i in range(len(nums1)):
+        # Note the key is the actual number
+        if nums1[i] not in my_dict:
+            my_dict[nums1[i]] = i
+
+    # Now traverse through nums2 and check if an element is present in the dictionary
+    # If yes, append it to the final_list
+    for item in nums2:
+        if item in my_dict:
+            final_list.append(item)
+
+    return list(set(final_list))
+
+# RUN TIME -> 35 ms
+def best_leet_code_sol(nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    return list(set(nums1) & set(nums2))
+
+nums1 = [1, 2, 2, 1]
+nums2 = [2, 2]
+print(intersection(nums1, nums2))
