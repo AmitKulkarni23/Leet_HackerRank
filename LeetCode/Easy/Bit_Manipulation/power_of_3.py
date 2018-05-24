5fd396f32d252f5defdc1411c6513ce669d5db4a
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-05-24 18:57:30 -0400

    Power of 3

---
+++ LeetCode/Easy/Bit_Manipulation/power_of_3.py
@@ -0,0 +1,35 @@
+def power_of_3_math(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    # n = 3 ^ i
+    # i = log3(n)
+    # i = log10(n) / log10(3)
+    # return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+
+
+def isPowerOfThree_integer_limitation(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    # Integer limitation
+    # We know that the max integer number held is 3 ^ 19
+    # There if the given number completely divides 3 ^ 19, then return true
+
+    return n > 0 and ( 3 ** 19 % n) == 0
+
+def isPowerOfThree_recursive(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    # print n
+    if n < 1:
+        return False
+    if n == 1:
+        return True
+    if n%3:
+        return False
+    return self.isPowerOfThree(n/3)
