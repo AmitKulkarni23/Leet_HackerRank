2dc2e2d480e7e2ca41a4b03f1c32286212e44e9f
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-06-05 15:26:05 -0400

    Number of segments in a string

---
+++ LeetCode/Easy/Strings/434_number_of_segments_in_string.py
@@ -0,0 +1,22 @@
+# Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+#
+# Please note that the string does not contain any non-printable characters.
+#
+# Example:
+#
+# Input: "Hello, my name is John"
+# Output: 5
+
+def countSegments(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    # Credits: https://leetcode.com/problems/number-of-segments-in-a-string/solution/
+    count = 0
+
+    for i in range(len(s)):
+        if (i == 0 or s[i-1] == " ") and s[i] != " ":
+            count += 1
+
+    return count
