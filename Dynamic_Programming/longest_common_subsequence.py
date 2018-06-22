dd3812ee5b59407416cfd640d3081ddf04480183
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-06-22 11:33:02 -0400

    Longest Common Subsequence Recursive Solution added

--- Dynamic_Programming/longest_common_subsequence.py
+++ Dynamic_Programming/longest_common_subsequence.py
@@ -25,8 +25,10 @@ def longest_common_subsequence(X, Y):
     """
     m = len(X)
     n = len(Y)
-    # Create a 2D array to store teh results
+    # Create a 2D array to store teh results of size (n + 1) x (m + 1)
     L = [[None] * (n + 1) for i in range(m+1)]
+
+    print(L)
 
     # Iterate through both teh strings
     for i in range(m+1):
@@ -38,15 +40,43 @@ def longest_common_subsequence(X, Y):
             else:
                 L[i][j] = max(L[i-1][j], L[i][j-1])
 
+    print("After everything L is ")
+    print(L)
+
     return L[m][n]
 
 
+def recursive_LCS(X, m, Y, n):
+    """
+    Recursive implementation of teh LCS problem
+
+    Inputs:
+    X - string
+    m - len(X)
+    Y - string
+    n - len(Y)
+    """
+
+    # Base Case
+    # If the lenght of either of teh strings is 0, return 0
+
+    if m == 0 or n == 0:
+        return 0
+
+    # Else, check the last characters of both teh strings
+    if X[m - 1] == Y[n - 1]:
+        # If teh last characters match
+        return 1 + recursive_LCS(X, m-1, Y, n-1)
+    else:
+        # Teh last characters do not match
+        return max(recursive_LCS(X, m, Y, n-1), recursive_LCS(X, m - 1, Y, n))
 
 
 # Examples
 
-X = "ABCDEFGHIJKL"
-Y = "IJKL"
+X = "AJK"
+Y = "IJK"
 
 
-print("The lenght of LCS is ", longest_common_subsequence(X,Y))
+# print("The lenght of LCS is ", longest_common_subsequence(X,Y))
+print("Recursive LCS ", recursive_LCS(X, len(X), Y, len(Y)))
