dc7371341c6ae256f04316509691d4e9d50df294
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-07-08 21:07:57 -0400

    x ^ y

---
+++ Misc(Math)/power_of_x.py
@@ -0,0 +1,28 @@
+# Write a program to calculate pow(x,n)
+# Given two integers x and n, write a function to compute xn. We may assume that x and n are small and overflow doesn’t happen.
+
+
+def power(x, y):
+    """
+    Function that cacluates x ^ y
+    Can work for -ve y
+    """
+    temp = 0.0
+    if y == 0:
+        # Anything raised to 0 is 1
+        return 1
+
+    temp = power(x, int(y / 2))
+    if y  % 2 == 0:
+        # If it is an even power
+        return temp * temp
+    else:
+        # Odd power
+        if y > 0:
+            return x * temp * temp
+        else:
+            return (temp * temp) / x
+# Examples:
+x = 3
+y = -4
+print(power(x, y))
