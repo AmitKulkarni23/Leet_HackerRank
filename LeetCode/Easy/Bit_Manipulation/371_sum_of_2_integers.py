b7469772e0d585f3c0a874214bf8bb1863acb2c2
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-07-12 21:09:54 -0400

    Sum of 2 numbers using bit manipulation

---
+++ LeetCode/Easy/Bit_Manipulation/371_sum_of_2_integers.py
@@ -0,0 +1,35 @@
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+#
+# Example:
+# Given a = 1 and b = 2, return 3.
+
+# 1 AND 1 = 1
+# 1 XOR 0 = 1
+
+# carry = and operation
+# residue = xor operation
+
+def getSum(a, b):
+    """
+    :type a: int
+    :type b: int
+    :rtype: int
+    """
+    # Corner cases
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+
+    while b != 0:
+
+        # Calculate carry
+        carry = a & b
+
+        # Assign the differnt bits to a
+        a = a ^ b
+
+        b = carry << 1
+
+    return a
+print(getSum(10, 12))
