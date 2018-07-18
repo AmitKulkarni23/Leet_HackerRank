d63e037a52b39f0f76863437a7d25b445eb9baf2
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-07-18 17:13:15 -0400

    Binary Search Tree

---
+++ LeetCode/Easy/Trees/98_validate_binary_search_tree.py
@@ -0,0 +1,69 @@
+# Given a binary tree, determine if it is a valid binary search tree (BST).
+#
+# Assume a BST is defined as follows:
+#
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+# Example 1:
+#
+# Input:
+#     2
+#    / \
+#   1   3
+# Output: true
+# Example 2:
+#
+#     5
+#    / \
+#   1   4
+#      / \
+#     3   6
+# Output: false
+# Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+#              is 5 but its right child's value is 4.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+        # Idea -> Start with range of min = None and max = None
+        # None indicates there is no min or max
+
+        # Branch left -> min = None and max = root.val
+        # Branch right -> min = root.val and max = None
+        #
+        # i.e when we branch left max gets updated, branch right and min gets updated
+        #
+        # Time Complexity -> O(n)
+        min = float("-inf")
+        max = float("inf")
+        return self.checkBST(root, min, max)
+
+
+    def checkBST(self, n, min, max):
+        """
+        A helper function
+        """
+        if n is None:
+            return True
+
+        if (min != None and n.val <= min) or (max != None and n.val >= max):
+            return False
+
+        if (not self.checkBST(n.left, min, n.val)) or (not self.checkBST(n.right, n.val, max)):
+            return False
+
+
+        return True
