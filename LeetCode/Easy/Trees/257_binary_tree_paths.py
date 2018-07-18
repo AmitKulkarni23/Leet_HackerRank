af6cb348ca868f4947b9a3323f5f46754a09e115
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-07-18 12:35:10 -0400

    All paths from root to leaf

---
+++ LeetCode/Easy/Trees/257_binary_tree_paths.py
@@ -0,0 +1,53 @@
+# Given a binary tree, return all root-to-leaf paths.
+#
+# Note: A leaf is a node with no children.
+#
+# Example:
+#
+# Input:
+#
+#    1
+#  /   \
+# 2     3
+#  \
+#   5
+#
+# Output: ["1->2->5", "1->3"]
+#
+# Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        final_list = []
+
+        if root == None:
+            return None
+
+        self.binary_tree_helper(root, final_list, "")
+        return final_list
+
+    def binary_tree_helper(self, root, final_list, path):
+        """
+        A helper function
+        """
+
+        if root.left == None and root.right == None:
+            final_list.append(path + str(root.val))
+
+        if root.left != None:
+            self.binary_tree_helper(root.left, final_list, path + str(root.val) + "->")
+
+        if root.right != None:
+            self.binary_tree_helper(root.right, final_list, path + str(root.val) + "->")
