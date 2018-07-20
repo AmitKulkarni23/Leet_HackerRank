7839fcbcb0df3a78b9bd1fc447455cb3adf036a8
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-07-20 15:04:50 -0400

    Average of all levels

---
+++ LeetCode/Easy/Trees/637_average_of_all_levels_bt.py
@@ -0,0 +1,54 @@
+# Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+# Example 1:
+# Input:
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# Output: [3, 14.5, 11]
+# Explanation:
+# The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+# Note:
+# The range of node's value is in the range of 32-bit signed integer.
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+
+        ans = []
+
+        if not root:
+            return ans
+
+        stack = [root]
+
+        while stack:
+            inter_level = []
+            value = 0.0
+
+            for node in stack:
+                value += node.val
+
+                if node.left:
+                    inter_level.append(node.left)
+
+                if node.right:
+                    inter_level.append(node.right)
+
+            ans.append(value / len(stack))
+
+            stack = inter_level
+
+        return ans
