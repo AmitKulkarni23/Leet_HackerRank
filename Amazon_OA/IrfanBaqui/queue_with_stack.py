69f1a9f7508587e826c1abed27ea469110693ee4
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2020-02-01 19:14:11 -0500

    Added implementation of queue with stack

---
+++ Amazon_OA/IrfanBaqui/queue_with_stack.py
@@ -0,0 +1,31 @@
+# Implement Queue With Stack
+# We will only do the dequeue function
+# We are only allowed one stack
+
+class Q:
+
+    def __init__(self, stack):
+        self.stack = stack
+
+    def dequeue(self):
+        if self.stack:
+            return self.recurse()
+
+    def recurse(self):
+        if len(self.stack) == 1:
+            return stack.pop()
+
+        cur_val = self.stack.pop()
+        result = self.recurse()
+        self.stack.append(cur_val)
+        return result
+
+stack = [1, 2, 3, 4]
+queue_obj = Q(stack)
+
+print(queue_obj.stack)
+print("Dequeue 1 = ", queue_obj.dequeue())
+print(queue_obj.stack)
+
+print("Dequeue 2 = ", queue_obj.dequeue())
+print(queue_obj.stack)
