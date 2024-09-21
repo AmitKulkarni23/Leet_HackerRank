f3439a3aef0be331fa3242392fdd0cfda7847b79
Amit Kulkarni <amitrkulkarni232@gmail.com>
2024-09-21 14:16:11 -0700

    Added inorder traversal

---
+++ LeetCode_Latest/Trees/Easy/94_binary_tree_inorder_traversal.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # Inorder traversal - Visit the left child + visit the node + visit the right child
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
